9af6fa4d07252d5b20c246de5681b3d3db289ea0
Federico Besenzoni <besen@hotmail.com>
2021-06-14 12:18:15 -0300

    rest server 2

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const express = require('express')
-const app = express()
-require('dotenv').config();
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(process.env.PORT,() => {
 
-    console.log('Escuchando puerto',process.env.PORT )
-})
+
+require('dotenv').config();
+
+const Server = require('./models/server')
+
+
+const server = new Server();
+
+
+server.listen();
+
+
+ 
+
+ 
